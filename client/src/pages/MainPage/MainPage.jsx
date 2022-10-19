2fb370ef19ed76ffd43ac59afc872f8f054cf200
bert7438 <bert7438@gmail.com>
2022-10-20 01:37:04 +0300

    first MainPage changes

--- client/src/pages/MainPage/MainPage.jsx
+++ client/src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
-const MainPage = () =>{
-    return(
-        <div>
-            <h1>Main Page</h1>
-        </div>
-    )
+const MainPage = () => {
+    return (<div className="container">
+            <div className="main-page">
+                <h4>Добавить пост:</h4>
+                <form className="form form-login">
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <input
+                                type="text"
+                                id="text"
+                                name="input"
+                                className="validate"/>
+                            <label htmlFor="input">Задача</label>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>)
 }
 
 export default MainPage
