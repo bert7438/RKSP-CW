4b0413359bda419fa1d01aca9dd5dd144247fe24
bert7438 <bert7438@gmail.com>
2022-10-22 23:00:56 +0300

    try to fix heroku deploy #5

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
+const path = require('path')
 const app = express()
-const PORT = 5000
+const PORT = app.listen(process.env.PORT || 5000)
 app.use(express.json({extended: true}))
 
 app.use('/api/auth', require('./routes/auth.route'))
 app.use('/api/post', require('./routes/post.route'))
+dotenv.config({ path: ".env" });
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/client/build")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname,"client","build","index.html"))
+    })
+}else{
+    app.get("/", (req, res) => {
+        res.send("Api running")
+    })
+}
+
 async function start() {
     try{
         await mongoose.connect('mongodb+srv://admin:admin@cluster0.c408rxf.mongodb.net/?retryWrites=true&w=majority', {
